e60293aa83e693076fa0ec18c83a2afe32d7e4cf
Chelsea <cberrios5114@gmail.com>
2023-11-13 22:28:12 -0500

    Added files

---
+++ script.js
@@ -0,0 +1,142 @@
+const gameData = {
+    "game": [
+      {
+        "name": "Five Nights at Freddy's",
+        "year": "Aug 2014",
+        "description": "Five Nights at Freddy's (FNaF) is a point-and-click survival horror game developed and published by Scott Cawthon. The player takes on the role of Mike Schmidt, a night security guard at a family pizzeria. Schmidt must complete his shifts without being confronted by the homicidal animatronic characters that wander the restaurant at night. The player has access to security cameras to monitor the animatronics throughout the shift, and a set of steel doors that can lock out the characters. Using the cameras and doors consumes the player's limited electricity, and draining all of the power causes these tools to become inoperable.",
+        "images": ["fnaf1.webp", "fnaf12.jpg", "fnaf13.jpg"],
+        "fontColor": "#ffffff",
+        "backgroundColor": "#a87356"
+      },
+      {
+        "name": "Five Nights at Freddy's 2",
+        "year": "Nov 2014",
+        "description": "Set in a fictional pizzeria, the player takes on the role of night security guard Jeremy Fitzgerald, who must defend themselves from the restaurant's hostile animatronic mascots while in their office. The player has access to security cameras throughout the restaurant to monitor animatronic activity. Wearing an animatronic mask allows the player to avoid being detected in most cases, though some animatronics are repelled through other methods. If an animatronic enters the office and the player is detected, they will be killed. As the game progresses, Atari-styled minigames and phone calls provide insight on the history of the restaurant.",
+        "images": ["fnaf2.webp", "fnaf22.webp", "fnaf23.jpg"],
+        "fontColor": "#ffffff",
+        "backgroundColor": "#a87ccc"
+      },
+      {
+        "name": "Five Nights at Freddy's 3",
+        "year": "Mar 2015",
+        "description": "The player takes on the role of an unnamed security guard who must defend themself from a decrepit animatronic called Springtrap that roams the attraction while battling hallucinations of other withered animatronics. In order to survive, the player must monitor the building's security cameras while maintaining additional systems, which will occasionally shut down. Unlike the two previous games, Five Nights at Freddy's 3 features only one animatronic capable of killing the player.",
+        "images": ["fnaf3.webp", "fnaf32.jpg", "back1.webp"],
+        "fontColor": "#000000",
+        "backgroundColor": "#f0f00e"
+      },
+      {
+        "name": "Five Nights at Freddy's 4",
+        "year": "Jul 2015",
+        "description": "Unlike previous installments, it is set in a child's bedroom rather than a security office. The player, who takes the role of a child, must defend themselves from variants of the four animatronics of the first game for four nights and then against Fredbear for the fifth night. If the player does not sufficiently monitor the four entrances—the bed, two side doors, and the closet—an animatronic will jump scare and kill them, and they must start the night over again. The player must rely on audio cues.",
+        "images": ["fnaf4.webp", "fnaf42.jpg", "fnaf43.jpeg"],
+        "fontColor": "#ffffff",
+        "backgroundColor": "#f00e38"
+      },
+      {
+        "name": "Five Nights at Freddy's: Sister Location",
+        "year": "Oct 2016",
+        "description": "As the title implies, the game does not take place at one of the Freddy Fazbear's Pizza restaurants featured in previous installments, but instead at a sister location called Circus Baby's Entertainment and Rental, which features its own set of animatronic characters, central to which is the titular Circus Baby. Players control a new employee who must perform maintenance work at the facility where the animatronics are stored while defending themselves from the animatronics, which become mobile and homicidal at night. Gameplay in Sister Location is drastically different from previous Five Nights at Freddy's games, as it allows players to move between rooms and requires different objectives to be carried out from night to night.",
+        "images": ["s1.jpg", "s2.jpg", "s3.jpg"],
+        "fontColor": "#000000",
+        "backgroundColor": "#ccbc7c"
+      },
+      {
+        "name": "Freddy Fazbear's Pizzeria Simulator ",
+        "year": " Dec 2017",
+        "description": "The player character is the new franchiser of the re-branded Fazbear Entertainment Inc. Chronologically set after the events of the third game. Initially marketed as a simulation game where the player manages their own franchised pizzeria, the game is gradually revealed to continue its predecessors' concept of surviving against hostile animatronic characters for multiple nights in a row and serves as a somewhat conclusion to the original six games that came before it.",
+        "images": ["ps1.jpg", "ps2.webp", "ps3.jpg"],
+        "fontColor": "#ffffff",
+        "backgroundColor": "#a87356"
+      },
+      {
+        "name": "Ultimate Custom Night",
+        "year": "Jun 2018",
+        "description": "The game allows the player character to choose from 50 characters from the first six main Five Nights at Freddy's games and FNaF World, and set their difficulty levels for the night from a minimum of 0 to a maximum of 20. During the night, the player must keep track of several mechanics, such as doors, ventilation systems, and air duct systems to avoid being attacked by the animatronics. The player can earn Faz-Coins to buy power-ups, plushies to stop certain animatronics, and a Death Coin to eliminate an animatronic of the player's choice from the current night. While the game lacks an explicit narrative, it is strongly implied to represent a form of Hell experienced by series antagonist William Afton following his final death as seen in Pizzeria Simulator.  ",
+        "images": ["ucn3.jpg", "ucn2.webp", "ucn.webp"],
+        "fontColor": "#ffffff",
+        "backgroundColor": "#a87ccc"
+      },
+      {
+        "name": "Five Nights at Freddy's: Help Wanted",
+        "year": "May 2019",
+        "description": "The game presents itself as The Freddy Fazbear Virtual Experience, produced for the fictitious corporative entity Fazbear Entertainment to help improve its public image after a series of debilitating lawsuits, following several incidents and disasters that allegedly occurred at their various locations. Gameplay is presented from the first-person perspective and focuses on a series of minigames based on previous games in the series, in which the player must evade attacks from sentient animatronics. The game's canon establishes that Fazbear Entertainment hired an unnamed indie developer to create a video game series based on the events of the previous seven games, before cutting ties with him, and commissioned a virtual reality game meant to make light of the rumors and convince players that those events were completely fictitious.",
+        "images": ["hw1.jpg", "hw2.jpg", "hw3.webp"],
+        "fontColor": "#000000",
+        "backgroundColor": "#f0f00e"
+      },
+      {
+        "name": "Five Nights at Freddy's AR: Special Delivery",
+        "year": "Nov 2019",
+        "description": " The game, which presents itself as an animatronic rental service created by the in-universe company Fazbear Entertainment, takes inspiration from the history of Five Nights at Freddy's across all media. The game features location-based augmented reality gameplay, similar to Pokémon Go. Numerous malfunctioning animatronics are sent to the player's home, which the player has to survive from. The player has numerous tools at their aid, including a flashlight and a controlled shocker. The player can also salvage animatronics for parts and send their own animatronics to their friends' homes. The game receives continuous updates adding new characters, skins for the animatronics, and new gameplay features.",
+        "images": ["ar1.jpg", "ar2.jpg", "ar3.webp"],
+        "fontColor": "#ffffff",
+        "backgroundColor": "#f00e38"
+      },
+      {
+        "name": "Five Nights at Freddy's: Security Breach",
+        "year": "Dec 2021",
+        "description": "The player takes control of a young boy named Gregory, who is locked in the Freddy Fazbear's Mega Pizzaplex, a large shopping mall filled with virus-infected pursuing animatronics and must complete numerous missions to survive the night in order to escape. The 1980s glam rock-styled animatronics found throughout the mall include Glamrock Freddy, Glamrock Chica, Montgomery Gator, Roxanne Wolf, the Daycare Attendant, the Glamrock Endoskeletons, the Wind-Up Music Man, and DJ Music Man. Gregory will also encounter other types of enemies, such as Vanessa, the mall's security guard who also turns out to be Vanny, a corrupt woman in a rabbit suit; and the S.T.A.F.F. robots that can alert enemies to Gregory's location if spotted.",
+        "images": ["sb1.jpg", "sb2.webp", "sb3.jpg"],
+        "fontColor": "#000000",
+        "backgroundColor": "#ccbc7c"
+      },
+    ]
+  };
+
+   function displayGame() {
+    const gameContainer = document.body;
+
+    gameData.game.forEach(project => {
+      const projectElement = document.createElement('div');
+      projectElement.classList.add('project');
+      projectElement.style.color = project.fontColor;
+      projectElement.style.backgroundColor = project.backgroundColor;
+
+      const projectName = document.createElement('h2');
+      projectName.textContent = `${project.name} (${project.year})`;
+
+      const projectDescription = document.createElement('p');
+      projectDescription.textContent = project.description;
+
+      const projectImages = project.images.map(image => ({
+        src: image,
+        opts: { caption: project.description }
+      }));
+
+      $(projectElement).on('click', function() {
+        $.fancybox.open(projectImages, {
+          loop: true, 
+          thumbs: {
+            autoStart: true, 
+          },
+        });
+      });
+
+      projectElement.appendChild(projectName);
+      projectElement.appendChild(projectDescription);
+
+      const imageContainer = document.createElement('div');
+      imageContainer.classList.add('image-container');
+
+      projectImages.forEach(image => {
+        const thumbnail = document.createElement('img');
+        thumbnail.src = image.src;
+        thumbnail.alt = project.name;
+        thumbnail.addEventListener('click', () => {
+          $.fancybox.open(projectImages, {
+            loop: true,
+            thumbs: {
+              autoStart: true,
+            },
+            index: projectImages.indexOf(image),
+          });
+        });
+        imageContainer.appendChild(thumbnail);
+      });
+
+      projectElement.appendChild(imageContainer);
+      gameContainer.appendChild(projectElement);
+    });
+  }
+
+  displayGame();
